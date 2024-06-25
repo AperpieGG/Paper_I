666293b03e9e16e09b46a8d2b1a473f0d3b6c225
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-06-25 13:35:05 +0100

    fix the path

--- Bias_analysis.py
+++ Bias_analysis.py
@@ -87,8 +87,8 @@ def read_bias_data(directory):
     sensitivity = get_sensitivity(directory)
     print(f'Calculating the read noise with sensitivity of {sensitivity}')
 
-    path = (f'/Users/u5500483/Downloads/Paper_I/Marana_updated_compare/'
-            f'Testing_CMOS_Linux/Images/json/Bias_Dark_Frames/Bias_{directory}/')
+    path = (f'/Users/u5500483/Downloads/Paper_I/Images/'
+            f'json/Bias_Dark_Frames/Bias_{directory}/')
 
     list_images = glob.glob(path + '*.fits')
     bias_values = []
